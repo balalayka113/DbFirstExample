065cf8cf1ecef688210672c8482846811c4bf567
and <and@_AND_>
2016-04-26 00:12:20 +0300

    Db first model has been added

---
+++ DbFirstExample/Product.Methods.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbFirstExample {
+    public partial class Product {
+        public override string ToString() {
+            return $"({Name})({Price})";
+        }
+    }
+}
--- DbFirstExample/Program.cs
+++ DbFirstExample/Program.cs
@@ -7,6 +7,20 @@ using System.Threading.Tasks;
 namespace DbFirstExample {
     class Program {
         static void Main(string[] args) {
+            using (ShopDb shopDb = new ShopDb()) {
+                Product product = new Product {
+                    Currency = new Currency { Name = "$" },
+                    Manufacturer = new Manufacturer { Name = "Dell" },
+                    Price = 100,
+                    Name = "monitor"
+                };
+                shopDb.Products.Add(product);
+                shopDb.SaveChanges();
+
+                shopDb.Products.ToList().ForEach(i => Console.WriteLine(i.ToString()));
+            }
+
+            Console.ReadKey();
         }
     }
 }
